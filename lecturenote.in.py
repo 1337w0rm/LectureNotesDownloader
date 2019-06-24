488d17b0f47f043d631f755a9bc29f3c38c0b821
Aditya <41179329+1337w0rm@users.noreply.github.com>
2019-06-24 16:39:14 +0530

    Completed the Project

--- lecturenote.in.py
+++ lecturenote.in.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
+import img2pdf 
+from PIL import Image 
 import requests
 import re
 import os
 
-print("Enter the URL of the note you want to download: ")
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+imagenames = []
+print(bcolors.BOLD + "Enter the URL of the note you want to download: ")
 url = input()
 
 site = requests.get(url)
@@ -17,24 +31,38 @@ totalPagesDiv = soup.find('div','book_buttom')
 totalPagesSpan = [span for span in totalPagesDiv.findAll('span')]
 totalPages = int(totalPagesSpan[2].text.strip(' / ')) + 1
 
-#for i in range(1,totalPages):
-siteA = requests.get("https://lecturenotes.in" + link + str(1))
-soupA = BeautifulSoup(siteA.content,'lxml')
-
-a = soupA.select_one("#pic" + str(1))
-
-pic = a['style']
-pic = pic.strip("background-image: url( ); background-size:cover;")
-pic = re.sub("\).*?px","",pic)
-
-pageLinks = "https://lecturenotes.in" + pic
-
-print("Enter the name of the PDF file")
+print(bcolors.BOLD + "Enter the name of the PDF file")
 dirName = input()
 
-if not os.path.exists('my_folder'):
-	os.makedirs(dirName)
+#Creating Directories
+if not os.path.exists(dirName):
+	os.makedirs(dirName + '/Images')
+	os.makedirs(dirName + '/PDF')
+
+print(bcolors.OKBLUE + "[+] Total Number of Pages are " + str(totalPages-1))
+for i in range(1,5):
+	siteA = requests.get("https://lecturenotes.in" + link + str(i))
+	soupA = BeautifulSoup(siteA.content,'lxml')
+
+	a = soupA.select_one("#pic" + str(i))
+
+	pic = a['style']
+	pic = pic.strip("background-image: url( ); background-size:cover;")
+	pic = re.sub("\).*?px","",pic)
+	pageLinks = "https://lecturenotes.in" + pic 
+	print(bcolors.OKGREEN + "[+] Page " + str(i) + " added to PDF")
+
+	#Downloading Images
+	r = requests.get(pageLinks)
+	with open(dirName + '/Images/' + str(i) + '.jpeg', 'wb') as f:
+		f.write(r.content)
+		f.close()
+	imagenames.append(dirName + '/Images/' + str(i) + '.jpeg')
 
 
+#Converting Image to PDF
+with open(dirName + '/PDF/' + dirName + ".pdf","wb") as f:
+	f.write(img2pdf.convert(imagenames))
+	f.close
 
-
+print(bcolors.BOLD + "Sucessfully Downloaded PDF")
